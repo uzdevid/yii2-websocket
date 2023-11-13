f7858fab17473b5f6fd7ec93e0a03587ba16b879
uzdevid <uzdevid@gmail.com>
2023-11-13 19:14:23 +0500

    Добавил поле

--- src/messages/Success.php
+++ src/messages/Success.php
@@ -7,9 +7,11 @@ use yii\base\Arrayable;
 
 class Success extends Message {
     public bool $success = true;
+    public string $method;
     public Arrayable|array|null $body;
 
-    public function __construct(Arrayable|array|null $body) {
+    public function __construct(string $method, Arrayable|array|null $body) {
+        $this->method = $method;
         $this->body = $body;
     }
 }
