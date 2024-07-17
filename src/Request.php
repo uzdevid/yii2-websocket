9a967104b93714d3a0222a656bd6ca43416a66c7
uzdevid <uzdevid@gmail.com>
2024-07-17 13:51:45 +0500

    Переопределил request->headers для того чтобы каждое соединение использовало свои загаловки

--- src/Request.php
+++ src/Request.php
@@ -1,4 +1,41 @@
 <?php
 namespace UzDevid\WebSocket;
 
-class Request extends \yii\web\Request { }
+use yii\web\HeaderCollection;
+
+class Request extends \yii\web\Request {
+    /**
+     * @return HeaderCollection
+     */
+    public function getHeaders(): HeaderCollection {
+        $_headers = new HeaderCollection();
+        if (function_exists('getallheaders')) {
+            $headers = getallheaders();
+            foreach ($headers as $name => $value) {
+                $_headers->add($name, $value);
+            }
+        } elseif (function_exists('http_get_request_headers')) {
+            $headers = http_get_request_headers();
+            foreach ($headers as $name => $value) {
+                $_headers->add($name, $value);
+            }
+        } else {
+            // ['prefix' => length]
+            $headerPrefixes = ['HTTP_' => 5, 'REDIRECT_HTTP_' => 14];
+
+            foreach ($_SERVER as $name => $value) {
+                foreach ($headerPrefixes as $prefix => $length) {
+                    if (strncmp($name, $prefix, $length) === 0) {
+                        $name = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, $length)))));
+                        $_headers->add($name, $value);
+                        continue 2;
+                    }
+                }
+            }
+        }
+
+        $this->filterHeaders($_headers);
+
+        return $_headers;
+    }
+}
